271b88d85f93b72339367927041fca81f65d6d88
rohit <rohitron0609@gmail.com>
2024-03-10 11:51:12 +0530

    Initial commit

---
+++ src/main/java/in/decx/crudUsingJPA/CrudUsingJpaApplication.java
@@ -0,0 +1,13 @@
+package in.decx.crudUsingJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsingJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/decx/crudUsingJPA/CrudUsingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.decx.crudUsingJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsingJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
